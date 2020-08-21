c528a8867c0ff05af97a7cb4f002aa5839125157
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2020-08-21 11:50:14 +0200

    fixed cart issue

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import SEO from '../components/seo'
+import SEO from '../components/SEO'
 
 import Skus from '../components/Products/Skus'
 import CartOverview from '../components/CartOverview'
 
 import { loadStripe } from '@stripe/stripe-js'
 import { CartProvider } from 'use-shopping-cart'
+
+//successUrl={`${window.location.origin}/page-2/`}
+//cancelUrl={`${window.location.origin}/`}
 
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
 
@@ -19,8 +22,7 @@ const CartExample = () => (
     <CartProvider
       mode="client-only"
       stripe={stripePromise}
-      successUrl={`${window.location.origin}/page-2/`}
-      cancelUrl={`${window.location.origin}/`}
+
       currency="USD"
       allowedCountries={['US', 'GB', 'CA']}
       billingAddressCollection={true}
