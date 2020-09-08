f4163aea8f3f0816e5b5413934e11840876d2a05
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2020-09-08 21:13:13 +0200

    working now

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,38 +1,38 @@
-import { silentAuth } from "./src/utils/auth"
-import React from 'react';
-import i18n from './i18next';
-import { I18nextProvider } from 'react-i18next';
+// import { silentAuth } from "./src/utils/auth"
+// import React from 'react';
+// import i18n from './i18next';
+// import { I18nextProvider } from 'react-i18next';
+//
+// class SessionCheck extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       loading: true,
+//     }
+//   }
+//
+//   handleCheckSession = () => {
+//     this.setState({ loading: false })
+//   }
+//
+//   componentDidMount() {
+//     silentAuth(this.handleCheckSession)
+//   }
+//
+//   render() {
+//     return (
+//       this.state.loading === false && (
+//         <React.Fragment>{this.props.children}</React.Fragment>
+//       )
+//     )
+//   }
+// }
 
-class SessionCheck extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-    }
-  }
+export { wrapPageElement, wrapRootElement } from './gatsby-ssr';
 
-  handleCheckSession = () => {
-    this.setState({ loading: false })
-  }
-
-  componentDidMount() {
-    silentAuth(this.handleCheckSession)
-  }
-
-  render() {
-    return (
-      this.state.loading === false && (
-        <React.Fragment>{this.props.children}</React.Fragment>
-      )
-    )
-  }
-}
-
-export { wrapPageElement } from './gatsby-ssr';
-
-export const wrapRootElement = ({ element }) => {
-
-  return <SessionCheck>
-          <I18nextProvider i18n={i18n}>{element}</I18nextProvider>
-          </SessionCheck>;
-};
+// export const wrapRootElement = ({ element }) => {
+//
+//   return <SessionCheck>
+//           <I18nextProvider i18n={i18n}>{element}</I18nextProvider>
+//           </SessionCheck>;
+// };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {PageContextProvider} from './PageContext';
+import PageContextProvider from './PageContext';
 import i18n from './i18next';
 import { I18nextProvider } from 'react-i18next';
 
@@ -12,9 +12,9 @@ import { I18nextProvider } from 'react-i18next';
 /**
  * Wrap all pages with a Translation provider and set the language on SSR time
  */
-// export const wrapRootElement = ({ element }) => {
-//   return <I18nextProvider i18n={i18n}>{element}</I18nextProvider>;
-// };
+export const wrapRootElement = ({ element }) => {
+  return <I18nextProvider i18n={i18n}>{element}</I18nextProvider>;
+};
 
 /**
  * Wrap all pages with a Translation provider and set the language on SSR time
